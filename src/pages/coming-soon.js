2353129bd8fa033dc3884b8806d460ad7d873775
Abimbola David <38018952+oluwadamilarey@users.noreply.github.com>
2021-11-04 19:58:46 +0100

    increaded time

--- src/pages/coming-soon.js
+++ src/pages/coming-soon.js
@@ -16,7 +16,7 @@ const ComingSoon = () => {
   }, [])
 
   const commingSoonTime = () => {
-    let endTime = new Date("November 22, 2021 17:00:00 PDT")
+    let endTime = new Date("November 30, 2021 17:00:00 PDT")
     let endTimeParse = Date.parse(endTime) / 1000
     let now = new Date()
     let nowParse = Date.parse(now) / 1000
